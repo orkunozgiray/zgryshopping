8a2685afd6f233b059d5e1c83e6b3c757de11b0c
orkunozgiray <36546670+orkunozgiray@users.noreply.github.com>
2021-01-25 06:25:01 -0900

    Navbar Icon Added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Nav } from './NavbarElements';
+import { Nav, NavLink, NavIcon, Bars } from './NavbarElements';
 
 const Navbar = () => {
     return (
         <>
-            <Nav>Navbar</Nav>
+            <Nav>
+                <NavLink to='/'>Tshirt</NavLink>
+                <NavIcon>
+                    <p>Menu</p>
+                    <Bars />
+                </NavIcon>
+            </Nav>
         </>
     );
 }
