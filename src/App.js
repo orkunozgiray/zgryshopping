d23fe5b2aa104aab89b7a3964e04acb1595cde36
orkunozgiray <36546670+orkunozgiray@users.noreply.github.com>
2021-01-25 12:40:02 -0900

    Products created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
 import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData } from './components/Products/data';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
-      <Navbar />
       <Hero />
+      <Products heading='Choose Your Clothes' data={productData} />
     </ Router>
   );
 }
---
+++ src/components/Products/data.js
@@ -0,0 +1,30 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+
+export const productData = [
+    {
+        img: product1,
+        alt: 'Tshirt',
+        name: 'Blue Tshirt',
+        desc: 'Medium size Blue Tshirt',
+        price: '30$',
+        button: 'Add to Cart'
+    },
+    {
+        img: product2,
+        alt: 'Tshirt',
+        name: 'Blue Tshirt',
+        desc: 'Medium size Blue Tshirt',
+        price: '30$',
+        button: 'Add to Cart'
+    },
+    {
+        img: product3,
+        alt: 'Tshirt',
+        name: 'Blue Tshirt',
+        desc: 'Medium size Blue Tshirt',
+        price: '30$',
+        button: 'Add to Cart'
+    }
+]
